02eabe4020481a8a47d74826106518f2879b8c9f
AbenezerM <abenezermulugeta41@gmail.com>
2022-12-01 22:31:38 -0600

    Push ideal solution

--- best-time-to-buy-and-sell-stock/src/App.java
+++ best-time-to-buy-and-sell-stock/src/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        int[] prices = {7, 1, 4, 3, 1};
+        int[] prices = {2, 4, 1};
         int profit = maxProfit(prices);
         System.out.println(profit);
     }
@@ -20,31 +20,32 @@ public class App {
             }
         }
 
-        return maxProfit;
 
         /*************************************************************
             My solution - (o(n))
 
-            int buyingAmount = prices[0];
-            int sellingPrice = 0;
-            int maximumProfit = 0;
-            int index = 0;
+                int buyPrice = prices[0];
+                int sellingPrice = 0;
+                int maxProfit = 0;
+                int index = 0;
 
-            for(int i = 0; i < prices.length; i++) {
-                if(prices[i] < buyingAmount){
-                    buyingAmount = prices[i];
-                    index = i;
+                for(int i = 0; i < prices.length; i++) {
+                    if(prices[i] < buyPrice){
+                        buyPrice = prices[i];
+                        index = i;
+                    }
                 }
-            }
 
-            for(int j = index; j < prices.length; j++){
-                if(prices[j] > sellingPrice){
-                    sellingPrice = prices[j];
-                    maximumProfit = sellingPrice - buyingAmount;
-                } 
-            }
+                for(int j = index; j < prices.length; j++){
+                    if(prices[j] > sellingPrice){
+                        sellingPrice = prices[j];
+                        maxProfit = sellingPrice - buyPrice;
+                    } 
+                }
 
-            return maximumProfit;
-        */
+                */
+
+
+        return maxProfit;
     }
 }
